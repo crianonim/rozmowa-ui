8cd4a36366636e4c7a88a4e75e6fd4f976161605
Jan Skowronski <crianonim@gmail.com>
2019-06-11 21:19:36 +0100

    Dialogs update

--- game-data/adventure-2/dialogs.mjs
+++ game-data/adventure-2/dialogs.mjs
@@ -1,6 +1,84 @@
 export default
     [
         {
+            id:"start",
+            intro:[
+                {text:`You wake up on the side of a road. Your head hurts and you don\'t remember anything. 
+                {{"The road seems to lead to some village, you have a feeling that the other way is trouble." "" looked-around ?}}` }
+            ],
+            options:[
+                {text:"Look at yourself.",go:"look-at-self"},
+                {text:"Look around.{{1 0 50 TEST ? a :=}}",if:"looked-around !",run:"1 looked-around := ; 1 TURN"},
+                {text:"Go towards the village",if:"looked-around",go:"village",run:"2 TURN"}
+            ]
+        },
+        {
+            id: "look-at-self",
+            run:"1 TURN",
+            intro: [
+                { text: "You look alright, no obvious damage" }
+            ],
+            options: [
+                { text: "Back", go: "exit" }
+            ]
+        },
+        {
+            id: "village",
+            intro: [
+                { text: `You are in the village. {{"There are some people around." "Everyone gone inside for the night" IS_DAY ?}}` }
+            ],
+            options: [
+                { text: `Talk to {{"an elderly man" "Bernie" met_bernie ! ?}} sitting on a bench on the green`, go: "talk_bernie" },
+                { text:`Go to the pond.`,go:"pond"},
+                { text: "Back to the road", run:"2 TURN", go: "exit" }
+            ]
+        },
+        {
+            id: "talk_bernie",
+            run:"met_bernie INC",
+            intro: [
+                { text: `"Hello again. How is it going? What can I do for you?" said Bernie`,if:"met_bernie 1 >" },
+                { text: `"Hi there. My name is Bernie. What can I do for you" said elderly man.` },
+
+            ],
+            options: [
+                { text: "Can you tell me what is this place?", go: "bernie_what_is_this_place" },
+                { text: "Nothing, I'm leaving", go: "exit" }
+            ]
+        },
+        {
+            id: "bernie_what_is_this_place",
+            intro: [
+                { text: "It's just a simple village." }
+            ],
+            options: [
+                { text: "Thanks.", go: "exit" }
+            ]
+        },
+        {
+            id: "pond",
+            intro: [
+                { text: "You find yourself by a pond." }
+            ],
+            options: [
+                { text: "Wash yourself.", if: "dirty", run:"2 TURN;0 dirty :=" },
+                { text: "Try to fish",go:"fishing"},
+                { text: "Back", go: "back" }
+            ]
+        },
+
+        {  
+            id: "fishing",
+            run :`2 TURN; 1 0 50 TEST ? catch := ; inventory.fish  catch INC_BY`,
+            intro: [
+                { text: "You caught a fish!",if:"catch" },
+                { text:"Sorry, no bonus"},
+            ],
+            options: [
+                { text: "Back", go: "exit" }
+            ]
+        },
+        {
             id: "basic",
             intro: [
                 { text: "Hello, my name is noone." }
@@ -23,39 +101,38 @@ export default
                 { text: "Hello to you to", run: "other.met 1 - other.met :=" },
                 { text: "I don't want to talk", go: "exit" }]
         },
-        
+
         {
-           
-            id:"adventure",
-            intro:[
-                {text:'You are standing in the village green. {{"You see a backpack" "" backpack ?}}'}
+            id: "adventure",
+            intro: [
+                { text: 'You are standing in the village green. {{"You see a backpack" "" backpack ?}}' }
             ],
-            options:[
-                {text:'Pick up the backpack',if:"backpack",run:"0 backpack :=; 1 inventory.backpack :="},
-                {text:'Sit on a bench and rest a bit',if:"stats.energy 10 <",run:"stats.energy INC;2 TURN"},
-                {text:'Do some paid work',if:'stats.energy 1 >', run:"stats.energy -2 INC_BY ; inventory.money 1 INC_BY; 5 TURN"},
-                {text:'Go to the tavern',go:"tavern",run:"1 TURN"},
-                {text:'Nothing...'},
+            options: [
+                { text: 'Pick up the backpack', if: "backpack", run: "0 backpack :=; 1 inventory.backpack :=" },
+                { text: 'Sit on a bench and rest a bit', if: "stats.energy 10 <", run: "stats.energy INC;2 TURN" },
+                { text: 'Go to the tavern', go: "tavern", run: "1 TURN" },
+                { text: 'Nothing...' },
             ]
         },
         {
-            id:"tavern",
-            intro:[
-                {text:'You are in the tavern'}
+            id: "tavern",
+            intro: [
+                { text: 'You are in the tavern' }
             ],
-            options:[
-                {text:'Talk to bartender',if:"talked_to_bartender !",go:'bartender'},
-                {text:'Leave',go:"adventure"},
+            options: [
+                { text: 'Do some paid work', if: 'stats.energy 1 >', run: "stats.energy -2 INC_BY ; inventory.money 1 INC_BY; 5 TURN" },
+                { text: 'Talk to bartender', if: "talked_to_bartender !", go: 'bartender' },
+                { text: 'Leave', go: "adventure" },
             ]
         },
         {
-            run:"1 TURN;talked_to_bartender INC",
-            id:"bartender",
-            intro:[
-                {text:'Hello, here\'s the sword to kill dragon' ,run:'inventory.sword INC'}
+            run: "1 TURN;talked_to_bartender INC",
+            id: "bartender",
+            intro: [
+                { text: 'Hello, here\'s the sword to kill dragon', run: 'inventory.sword INC' }
             ],
-            options:[
-                {text:'Leave',go:"adventure"},
+            options: [
+                { text: 'Leave', go: "exit" },
             ]
         }
     ]
--- game-data/adventure-2/index.mjs
+++ game-data/adventure-2/index.mjs
@@ -4,12 +4,13 @@ import rozmowa from 'rozmowa/index.mjs';
 const screept=rozmowa.screept;
 console.log("ROZMOWA",rozmowa);
 
-const dialogName = "adventure";
+const dialogName = "start";
 
-const ctx = { other: { met: -1 }, turn: 0, score: 0, backpack: 1, inventory: { money: 5, sword:0}, stats:{energy:10} }
+const ctx = { other: { met: -1 }, turn: 25, score: 0, backpack: 1, inventory: { money: 5, sword:0}, stats:{energy:10} ,dirty:true}
 
+const TURNS_PER_HOUR=4;
 function status() {
-    let s=`It is currently {{turn}} turn. You have {{score}} points.
+    let s=`It is currently {{turn}} turn. Time is {{HOUR}}. It's {{"day" "night" IS_DAY ?}} You have {{score}} points.
      You have: ${ Object.entries(ctx.inventory).filter(entry=>entry[1]).map(entry=>entry[0]+":"+entry[1]).join(", ") }
      Energy: {{stats.energy}}
      `
@@ -30,6 +31,20 @@ function init(){
               console.log("Turn passed, new turn ",++ctx.turn);
           }
       })
+      screept.addVerb("HOUR",0,()=>{
+          return ((ctx.turn / TURNS_PER_HOUR)>>0)%24;
+      })
+      screept.addVerb("IS_DAY",0,()=>{
+          let hour=((ctx.turn / TURNS_PER_HOUR)>>0)%24;
+          if (hour>5 && hour <20) return true;
+          return false;
+      })
+      //take percentage and return true or false 
+      screept.addVerb("TEST",1,(a)=>{
+        let roll=Math.random()*100;
+        return roll<a.value;
+    })
+      
 }
 export default{
     dialogs,
